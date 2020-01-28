17be90b5f2437b6bc15eb92eb0c888bdc57eb239
seanmoylan <sean.cheezy.moylan@gmail.com>
2020-01-28 16:32:21 +0000

    Made changes to user class

--- Spot Finder App/app/src/main/java/com/seanmoylan/myapplication/User.java
+++ Spot Finder App/app/src/main/java/com/seanmoylan/myapplication/User.java
@@ -3,6 +3,8 @@ package com.seanmoylan.myapplication;
 public class User {
     int id;
     String name;
+    String email;
+    String password;
     float latitude;
     float longitude;
 
@@ -10,9 +12,11 @@ public class User {
 
     }
 
-    public void User(int id, String name, float latitude, float longitude){
+    public void User(int id, String name, String password, String email, float latitude, float longitude){
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.password = password;
         this.latitude = latitude;
         this.longitude = longitude;
     }
