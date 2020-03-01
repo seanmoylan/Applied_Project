468822f2af75e73e37ec6533ab8b46c45188a8a5
seanmoylan <sean.cheezy.moylan@gmail.com>
2020-03-01 19:32:57 +0000

    Got server and database working together and sending back user when given username

--- Server/FlaskServer.py
+++ Server/FlaskServer.py
@@ -8,34 +8,41 @@ Adapted partly from: https://android.jlelse.eu/handmade-backend-for-android-app-
 import json
 import flask as fl
 from flask import request, jsonify, make_response
-from pymongo import MongoClient
+from flask_mongoengine import MongoEngine
 
 
-
-client = MongoClient()
 
 
 # Flask instance
 app = fl.Flask(__name__)
 
-
+# Connect to mongo and initialize
+db = MongoEngine()
+db.init_app(app)
 
 # Root directory
 @app.route("/")
 def index():
+    
+    User(username="cheezy23", email="seanmoylan1@icloud.com", password="1234").save()
+
     return "Server running..."
 
 # GET all users
 @app.route('/users')
 def get_users():
+    user = User.objects.get(username="cheezy23")
+
     with open("jsonfiles/user.json") as f:
         data = json.loads(f.read())
-    return make_response(data)
+    return jsonify(user)
 
 # POST specific user 
 @app.route('/users/<user>')
 def create_user(user):
-    return "Hello {user}"
+    username = user
+    user = User.objects.get(username=username)
+    return jsonify(user)
 
 # GET all locations
 @app.route('/locations')
@@ -59,4 +66,23 @@ def get_location(locationId):
     return "Specific location %s" %locationId
 
 
-#app.run(debug=True)
+
+
+
+# DATABASE
+
+
+
+
+class Location(db.Document):
+    latitude = db.FloatField(required=True)
+    longitude = db.FloatField(required=True)
+    title = db.StringField(required=True)
+    description = db.StringField()
+    spot_type = db.StringField()
+
+
+class User(db.Document):
+    username = db.StringField(unique=True, required=True)
+    email = db.EmailField(unique=True, required=True)
+    password = db.StringField(required=True)
