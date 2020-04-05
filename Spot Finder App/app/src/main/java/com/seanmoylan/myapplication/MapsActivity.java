6b419a665224d736b9f066b43d3b2b0758ba7445
seanmoylan <sean.cheezy.moylan@gmail.com>
2020-04-05 14:00:55 +0100

    Successfully calling POST locations to server

--- Spot Finder App/app/src/main/java/com/seanmoylan/myapplication/MapsActivity.java
+++ Spot Finder App/app/src/main/java/com/seanmoylan/myapplication/MapsActivity.java
@@ -50,7 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int FINE_LOCATION_REQUEST_CODE = 1;
 
     // This bundle is used to pass data to the SaveLocations page
-    Bundle bundle = new Bundle();
+    Bundle coordinatesBundle = new Bundle();
 
     private List<Location> locations;
 
@@ -65,15 +65,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         saveBtn = findViewById(R.id.saveBtn);
-        saveBtn.setClickable(false);
+        saveBtn.setVisibility(View.GONE);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 // Create Intent to start save location activity and pass coordinates
                 Intent saveActivityIntent = new Intent(getApplicationContext(), SaveLocation.class);
-
-
-                saveActivityIntent.putExtras(bundle);
+                saveActivityIntent.putExtras(coordinatesBundle);
                 startActivity(saveActivityIntent);
             }
         });
@@ -91,35 +90,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Toast.makeText(this, "Error retrieving current location", Toast.LENGTH_LONG);
         }
 
-
+        // Load locations on a seperate thread
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 loadLocations();
             }
         };
-
         Thread locate = new Thread(runnable);
         locate.start();
-
-
-
-
-
-
-
 
     }
 
     private void checkPermission(String permission, int requestCode) {
 
+        // Request permissions needed from user
         String[] perm = new String[]{permission};
 
-
         if(ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED){
-            // Request the permission
             ActivityCompat.requestPermissions(MapsActivity.this, new String[]{permission}, FINE_LOCATION_REQUEST_CODE);
-
         } else {
             Toast.makeText(this, "Permission is already granted", Toast.LENGTH_LONG);
         }
@@ -130,18 +119,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        LatLng sy = new LatLng(-35, 252);
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker near sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sy));
-
-
     }
 
     private void loadLocations() {
@@ -207,15 +184,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             LatLng pos = new LatLng(loc.getLatitude(), loc.getLongitude());
             mMap.addMarker(new MarkerOptions().position(pos).title(loc.getTitle()));
 
-            // Focus in on specific location
-            if(loc.getTitle().contains("NUIG")){
-                CameraUpdate location = CameraUpdateFactory.newLatLngZoom(myLocation, 15);
-                mMap.addMarker(new MarkerOptions().position(myLocation).title("Current Location"));
-                mMap.animateCamera(location);
-            }
 
-            bundle.putDouble("latitude", myLocation.latitude);
-            bundle.putDouble("longitude", myLocation.longitude);
+            CameraUpdate location = CameraUpdateFactory.newLatLngZoom(myLocation, 15);
+            mMap.addMarker(new MarkerOptions().position(myLocation).title("Current Location"));
+            mMap.animateCamera(location);
+
+            // Add coordinates to bundle
+            coordinatesBundle.putDouble("latitude", myLocation.latitude);
+            coordinatesBundle.putDouble("longitude", myLocation.longitude);
+
+            // Allow user to see saveBtn once the current location has loaded
+            saveBtn.setVisibility(View.VISIBLE);
         }
 
 
--- Spot Finder App/app/src/main/java/com/seanmoylan/myapplication/SaveLocation.java
+++ Spot Finder App/app/src/main/java/com/seanmoylan/myapplication/SaveLocation.java
@@ -1,12 +1,20 @@
 package com.seanmoylan.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.seanmoylan.myapplication.Classes.Location;
+import com.seanmoylan.myapplication.Classes.Login;
 
 public class SaveLocation extends AppCompatActivity {
 
@@ -17,7 +25,11 @@ public class SaveLocation extends AppCompatActivity {
     private Button cancelBtn;
     private Button saveBtn;
 
+    Retrofit retrofit;
+
     Location newLocation;
+    double latitude, longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +42,16 @@ public class SaveLocation extends AppCompatActivity {
         titleTxt = findViewById(R.id.saveTitleTxt);
         descriptionTxt = findViewById(R.id.descriptionTxt);
 
+        // Stop the user from editing the coordinates
+        latitudeTxt.setFocusable(false);
+        longitudeTxt.setFocusable(false);
 
-
-
-        // TODO: Fix this code
         if(populateCoordinates()){
-            latitudeTxt.setText(newLocation.getLatitude().toString());
-            longitudeTxt.setText(newLocation.getLongitude().toString());
+            latitudeTxt.setText(String.valueOf(latitude));
+            longitudeTxt.setText(String.valueOf(longitude));
         }
+
+        saveLocation();
     }
 
     private boolean populateCoordinates() {
@@ -45,11 +59,49 @@ public class SaveLocation extends AppCompatActivity {
 
         if (dataRecieved != null)
         {
-            newLocation.setLatitude(dataRecieved.getDouble("latitude"));
-            newLocation.setLongitude(dataRecieved.getDouble("longitude"));
+            latitude = dataRecieved.getDouble("latitude");
+            longitude = dataRecieved.getDouble("longitude") ;
             return true;
         }
 
         return false;
+    }
+
+    private void saveLocation(){
+
+        // Test Location
+        Location l = new Location();
+        l.setLongitude(2.23523);
+        l.setLatitude(3.23523);
+        l.setTitle("Hello World");
+        l.setDescription("Some generic text");
+        l.setType("stair");
+
+        // Set up retrofit instance for sending POST to server
+        // Retrofit and Gson
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:5000/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        FlaskAPI flaskApi = retrofit.create(FlaskAPI.class);
+        Call<Location> call = flaskApi.createLocation(l);
+
+        call.enqueue(new Callback<Location>() {
+            @Override
+            public void onResponse(Call<Location> call, Response<Location> response) {
+                System.out.println("Code: " + response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Location> call, Throwable t) {
+                System.out.println("Failure response: "+t.getMessage());
+            }
+        });
+
     }
 }
